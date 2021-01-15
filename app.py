4a71be2e89ed4559ced01d598c44173bfe460057
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-15 10:36:47 -0300

    criando pacote resources

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+
+from resources.hotel import Hoteis
 
 app = Flask(__name__)
 api = Api(app)
-
-hoteis = [
-    {
-        'hotel_id': 'alpha',
-        'nome': 'Alpha Hotel',
-        'estrelas': 4.3,
-        'diaia': 423.34,
-        'cidade': 'Rio'
-    },
-    {
-        'hotel_id': 'Paladins',
-        'nome': 'Cundi Hotel',
-        'estrelas': 3.7,
-        'diaia': 200.00,
-        'cidade': 'Rio'
-    },
-    {
-        'hotel_id': 'Fiulad',
-        'nome': 'GUloia Hotel',
-        'estrelas': 1.5,
-        'diaia': 54,
-        'cidade': 'Montes Claros'
-    }
-]
-
-
-class Hoteis(Resource):
-    def get(self):
-        return {'Hoteis': hoteis}
-
 
 api.add_resource(Hoteis, '/hoteis')
 
